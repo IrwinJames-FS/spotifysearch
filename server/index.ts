f3fba14f2e9113f607b885df4182555ccc65f49f
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-04-13 13:27:40 -0400

    #3 Started a react application.

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,18 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { greenlg } from './utils/cx';
 import api from './api';
+import passport = require('passport');
+import './strategies/spotify';
+
 dotenv.config();
 const app = express();
 
 const {PORT} = process.env;
 
+app.get('/auth', passport.authenticate('spotify'));
+app.get('/auth', passport.authenticate('spotify', {failureRedirect: '/auth'}), (req, res) => {
+	res.redirect('http://localhost:3000');
+});
 app.use('/api', api);
 app.listen(PORT, ()=>greenlg`[express] Server is listening on port ${PORT}`)
---
+++ server/models/User.ts
@@ -0,0 +1,31 @@
+import { Model, Schema, model } from "mongoose";
+
+const schema = new Schema<UserDocument, UserModel>({
+	_id: {
+		type: String,
+		required: [true, 'An ID must be provided']
+	},
+	accessToken: {
+		type: String,
+		required: [true, 'An access token was not provided'],
+	},
+	refreshToken: {
+		type: String,
+		required: [true, 'A refresh token was not provided'],
+	},
+	expires: {
+		type: Number,
+		required: [true, 'An expiration value must be provided']
+	}
+})
+
+export interface UserDocument extends Document {
+	_id: string,
+	accessToken: string,
+	refreshToken: string,
+	expires: number,
+}
+
+export interface UserModel extends Model<UserDocument> {};
+
+export const User  = model<UserDocument, UserModel>('User', schema);
